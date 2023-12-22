158efe2b6792a3911de0876f6804f4be27a5e4b4
Daniel Wai <daniel.wai6@gmail.com>
2023-12-22 11:33:17 -0500

    Minor update to labels to show target vs executed time and water

--- coffee/brewhelper/views.py
+++ coffee/brewhelper/views.py
@@ -5,13 +5,13 @@ from .models import brewstep
 def home(request):
     steps = brewstep.objects.all()
 
-    total_water = 0
-    total_time = 0
+    target_total_water = 0
+    target_total_time = 0
 
     for step in steps:
-        total_water += step.change_in_water
-        total_time += step.change_in_time
-        step.total_time = total_time
-        step.total_water = total_water
+        target_total_water += step.change_in_water
+        target_total_time += step.change_in_time
+        step.target_total_time = target_total_time
+        step.target_total_water = target_total_water
 
     return render(request, 'home.html', {'steps': steps})
