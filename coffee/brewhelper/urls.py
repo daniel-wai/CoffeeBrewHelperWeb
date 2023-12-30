11655bb0f28dffaf7d51f0a8dedb10ad961ef1e2
Daniel Wai <daniel.wai6@gmail.com>
2023-12-30 15:42:09 -0500

    Fixed issue where selected method and recipe are not retained when page is refreshed or the select button is hit

--- coffee/brewhelper/urls.py
+++ coffee/brewhelper/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('get-recipes', views.get_recipes, name='get_recipes')
 ]
